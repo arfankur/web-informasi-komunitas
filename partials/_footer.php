4b882611a9d1fa94f32b8a9dc456857eaa652a74
arfankur <arfankurnianto29@gmail.com>
2021-06-09 14:34:57 +0700

    final commit

--- partials/_footer.php
+++ partials/_footer.php
@@ -1,4 +1,4 @@
-<div class="bg-white ring-1 inline-block ring-gray-300 mt-80 lg:fixed w-full bottom-0">
+<div class="bg-white ring-1 inline-block ring-gray-300 mt-80 fixed w-full bottom-0">
     <!-- <hr class="ring-1 ring-gray-100"> -->
     <div class="font-bold text-center  py-3">KerkoemWEB
         <?= date('Y') ?>
